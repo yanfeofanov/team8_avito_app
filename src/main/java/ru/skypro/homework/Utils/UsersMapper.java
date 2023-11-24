3e5cbfa3cfa693f05c795f7246c01c25c0f958f2
verzhik <verzhik11@gmail.com>
2023-11-24 22:01:32 +0530

    рефакторинг маппинга для Register

--- src/main/java/ru/skypro/homework/Utils/UsersMapper.java
+++ src/main/java/ru/skypro/homework/Utils/UsersMapper.java
@@ -1,7 +1,11 @@
 package ru.skypro.homework.Utils;
 
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.skypro.homework.dto.*;
 import ru.skypro.homework.model.AvitoUser;
 
@@ -10,7 +14,14 @@ public interface UsersMapper {
 
     UserDto mapToUserDto(AvitoUser user);
 
-    void mapToUser(UpdateUserDto updateUserDto, @MappingTarget AvitoUser user);
+
+    AvitoUser mapToUser(UpdateUserDto updateUserDto, @MappingTarget AvitoUser user);
 
     UpdateUserDto mapToUpdate(AvitoUser user);
+
+    @Mapping(source = "username", target = "email")
+    @Mapping(target = "password", expression = "java(passwordEncoder.encode(register.getPassword()))")
+    AvitoUser mapToRegister(Register register, @Context PasswordEncoder passwordEncoder);
+
+
 }
